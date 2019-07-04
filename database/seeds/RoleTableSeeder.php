cd4faa2ab27317807312211d0a86015cc7adf613
3º Sgt Lucas <sgtlucas@badmapcmo.eb.mil.br>
2019-07-04 15:45:01 -0400

    Adicionado os Menus para Perfil

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -31,32 +31,46 @@ class RoleTableSeeder extends Seeder
             switch ($role) {
                 case 'super-admin':
                 
+                $rhp->givePermissionTo('militares-menu');
                 $rhp->givePermissionTo('militares-add');
                 $rhp->givePermissionTo('militares-list');
                 $rhp->givePermissionTo('militares-edit');
 
+                $rhp->givePermissionTo('veiculos-menu');
                 $rhp->givePermissionTo('veiculos-add');
                 $rhp->givePermissionTo('veiculos-list');
                 $rhp->givePermissionTo('veiculos-edit');
+
+                $rhp->givePermissionTo('consultas-menu');
                 $rhp->givePermissionTo('consultas-aut');
                 $rhp->givePermissionTo('consultas-ped');
+
 
                 $rhp->givePermissionTo('cracha-aut');
                 $rhp->givePermissionTo('cracha-ped');
                 $rhp->givePermissionTo('cracha-vtr');
 
+                $rhp->givePermissionTo('relatorios-menu');
                 $rhp->givePermissionTo('relatorios-aut');
                 $rhp->givePermissionTo('relatorios-hrs');
                 $rhp->givePermissionTo('relatorios-mil');
 
+                $rhp->givePermissionTo('config-om-menu');
                 $rhp->givePermissionTo('config-om-add');
                 $rhp->givePermissionTo('config-om-edit');
                 $rhp->givePermissionTo('config-om-list');
 
+                $rhp->givePermissionTo('config-usuario-menu');
+                $rhp->givePermissionTo('config-usuario-add');
+                $rhp->givePermissionTo('config-usuario-edit');
+                $rhp->givePermissionTo('config-usuario-list');
+
+                $rhp->givePermissionTo('config-vtr-menu');
                 $rhp->givePermissionTo('config-vtr-add');
                 $rhp->givePermissionTo('config-vtr-edit');
                 $rhp->givePermissionTo('config-vtr-list');
 
+                $rhp->givePermissionTo('config-veiculos-menu');
                 $rhp->givePermissionTo('config-veiculos-tipo');
                 $rhp->givePermissionTo('config-veiculos-marca');
                 $rhp->givePermissionTo('config-veiculos-modelo');
@@ -69,14 +83,17 @@ class RoleTableSeeder extends Seeder
 
                 case 'administrador':
 
+                $rhp->givePermissionTo('militares-menu');
                 $rhp->givePermissionTo('militares-add');
                 $rhp->givePermissionTo('militares-list');
                 $rhp->givePermissionTo('militares-edit');
 
+                $rhp->givePermissionTo('veiculos-menu');
                 $rhp->givePermissionTo('veiculos-add');
                 $rhp->givePermissionTo('veiculos-list');
                 $rhp->givePermissionTo('veiculos-edit');
 
+                $rhp->givePermissionTo('consultas-menu');
                 $rhp->givePermissionTo('consultas-aut');
                 $rhp->givePermissionTo('consultas-ped');
 
@@ -84,10 +101,12 @@ class RoleTableSeeder extends Seeder
                 $rhp->givePermissionTo('cracha-ped');
                 $rhp->givePermissionTo('cracha-vtr');
 
+                $rhp->givePermissionTo('relatorios-menu');
                 $rhp->givePermissionTo('relatorios-aut');
                 $rhp->givePermissionTo('relatorios-hrs');
                 $rhp->givePermissionTo('relatorios-mil');
 
+                $rhp->givePermissionTo('config-vtr-menu');
                 $rhp->givePermissionTo('config-vtr-add');
                 $rhp->givePermissionTo('config-vtr-edit');
                 $rhp->givePermissionTo('config-vtr-list');
@@ -100,14 +119,17 @@ class RoleTableSeeder extends Seeder
 
                 case 'operador':
 
+                $rhp->givePermissionTo('militares-menu');
                 $rhp->givePermissionTo('militares-add');
                 $rhp->givePermissionTo('militares-list');
                 $rhp->givePermissionTo('militares-edit');
 
+                $rhp->givePermissionTo('veiculos-menu');
                 $rhp->givePermissionTo('veiculos-add');
                 $rhp->givePermissionTo('veiculos-list');
                 $rhp->givePermissionTo('veiculos-edit');
 
+                $rhp->givePermissionTo('relatorios-menu');
                 $rhp->givePermissionTo('relatorios-aut');
                 $rhp->givePermissionTo('relatorios-mil');
 
