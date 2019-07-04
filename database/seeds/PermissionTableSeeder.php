89758e9e16dfaeb3fb290e3a38e55d6eb08a4979
3º Sgt Lucas <sgtlucas@badmapcmo.eb.mil.br>
2019-07-04 15:44:37 -0400

    Adicionado Permissão para Menus

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -17,14 +17,17 @@ class PermissionTableSeeder extends Seeder
     public function run()
     {
     	$permissions = [
+			'militares-menu',
     		'militares-add',
     		'militares-list',
     		'militares-edit',
-    		
+			
+			'veiculos-menu',
     		'veiculos-add',
     		'veiculos-list',
     		'veiculos-edit',
 
+			'consultas-menu',
     		'consultas-aut',
     		'consultas-ped',
 
@@ -32,18 +35,27 @@ class PermissionTableSeeder extends Seeder
     		'cracha-ped',
     		'cracha-vtr',
 
+			'relatorios-menu',
     		'relatorios-aut',
     		'relatorios-hrs',
     		'relatorios-mil',
 
+			'config-om-menu',
     		'config-om-add',
     		'config-om-edit',
     		'config-om-list',
 
+			'config-usuario-menu',
+    		'config-usuario-add',
+    		'config-usuario-edit',
+    		'config-usuario-list',
+
+			'config-vtr-menu',
     		'config-vtr-add',
     		'config-vtr-edit',
     		'config-vtr-list',
 
+    		'config-veiculos-menu',
     		'config-veiculos-tipo',
     		'config-veiculos-marca',
     		'config-veiculos-modelo',
