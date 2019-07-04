04ddbaa8cfad8c73abf9a5e80326a0f744ca82f6
3º Sgt Lucas <sgtlucas@badmapcmo.eb.mil.br>
2019-07-04 15:39:23 -0400

    Código comentado e segurança implementada

--- app/Http/Controllers/Sys/VeiculosController.php
+++ app/Http/Controllers/Sys/VeiculosController.php
@@ -4,72 +4,63 @@ namespace App\Model\Sys;
 
 namespace App\Http\Controllers\Sys;
 
+/* Vendors Laravel */
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\QueryException;
 use App\Http\Controllers\Controller;
+use DB;
 
+/* Models SIGAPS */
 use App\Model\Sys\Cad_tipo_automovel;
 use App\Model\Sys\Cad_automovel;
+use App\Model\Sys\Cad_militar;
 use App\Model\Sys\Cad_operacao;
 use App\Model\Sys\Cad_logs;
-use DB;
+
 
 class VeiculosController extends Controller
 {
-	public function create()
-	{
-		$tp_veiculo = Cad_tipo_automovel::where('id', '!=', 3)->orderBy('nome')->get();;
-		return view('sys.veiculos.cadastro', compact('tp_veiculo'));
-	}
+	/*
+    |--------------------------------------------------------------------------
+    | VIEWS 
+    |--------------------------------------------------------------------------
+    | Retornam as páginas referente ao CRUD do VEICULO 
+    | HTML disponível em resources/views/sys/veiculos
+	*/
 
 	public function index()
 	{
+		/* Listagem de Veiculos */
+		$this->authorize('list', Cad_automovel::class);
 		return view('sys.veiculos.listagem');
 	}
 
-	public function criar_log($id_operacao, $id_militar, $id_veiculo, $id_operador, $endereco_ip)
+	public function create()
 	{
-		$log = new Cad_logs;
-		$log->id_operacao = $id_operacao;
-		$log->id_militar = $id_militar;
-		$log->id_veiculo = $id_veiculo;
-		$log->id_operador = $id_operador;
-		$log->data_hora = date('Y-m-d H:i');
-		$log->endereco_ip = $endereco_ip;
-		$log->save();
+		/* Cadastro de Veiculos */
+		$this->authorize('create', Cad_automovel::class);
+		$tp_veiculo = Cad_tipo_automovel::where('id', '!=', 3)->orderBy('nome')->get();
+		return view('sys.veiculos.cadastro', compact('tp_veiculo'));
 	}
 
 	public function show($id)
 	{
-
+		/* Editar dados do Veículo */
 		$veiculo = Cad_automovel::findOrFail($id);
-
-		$log = DB::table('cad_logs')
-			->select('users.name', 'cad_logs.data_hora', 'cad_operacao.descricao', 'cad_operacao.evento')
-			->join('users', 'cad_logs.id_operador', '=', 'users.id')
-			->join('cad_automovel', 'cad_logs.id_veiculo', '=', 'cad_automovel.id')
-			->join('cad_operacao', 'cad_logs.id_operacao', '=', 'cad_operacao.id')
-			->where('cad_automovel.id', '=', $id)
-			->orderBy('cad_logs.id', 'desc')
-			->get();
-
-		$evento  = Cad_operacao::all();
-		$tp_veiculo = Cad_tipo_automovel::all();
-		$marca = DB::table('cad_marca')->where('tipo_id', '=', $veiculo->tipo_id)->get();
-		$modelo = DB::table('cad_modelo')->where('marca_id', '=', $veiculo->marca_id)->get();
-		$militar = DB::table('cad_militar')
-			->select(
-				'cad_militar.nome',
-				'cad_militar.nome_guerra',
-				'cad_militar.ident_militar',
-				'cad_militar.cnh',
-				'cad_militar.cnh_cat',
-				'cad_militar.cnh_venc',
-				'cad_posto.nome as posto_nome',
-				'cad_om.nome as om_nome'
-			)
+		$militar = Cad_militar::select(
+			'cad_militar.nome',
+			'cad_militar.nome_guerra',
+			'cad_militar.ident_militar',
+			'cad_militar.cnh',
+			'cad_militar.cnh_cat',
+			'cad_militar.cnh_venc',
+			'cad_militar.om_id',
+			'cad_posto.nome as posto_nome',
+			'cad_om.nome as om_nome'
+		)
 			->join(
 				'cad_posto',
 				'cad_militar.posto',
@@ -84,10 +75,45 @@ class VeiculosController extends Controller
 			)
 			->where('cad_militar.id', '=', $veiculo->militar_id)
 			->first();
+
+		$this->authorize('edit', $militar);
+		$log = DB::table('cad_logs')
+			->select('users.name', 'cad_logs.data_hora', 'cad_operacao.descricao', 'cad_operacao.evento')
+			->join('users', 'cad_logs.id_operador', '=', 'users.id')
+			->join('cad_automovel', 'cad_logs.id_veiculo', '=', 'cad_automovel.id')
+			->join('cad_operacao', 'cad_logs.id_operacao', '=', 'cad_operacao.id')
+			->where('cad_automovel.id', '=', $id)
+			->orderBy('cad_logs.id', 'desc')
+			->get();
+		$evento  = Cad_operacao::all();
+		$tp_veiculo = Cad_tipo_automovel::all();
+		$marca = DB::table('cad_marca')->where('tipo_id', '=', $veiculo->tipo_id)->get();
+		$modelo = DB::table('cad_modelo')->where('marca_id', '=', $veiculo->marca_id)->get();
 		return view('sys.veiculos.editar', compact('evento', 'log', 'militar', 'veiculo', 'tp_veiculo', 'marca', 'modelo'));
 	}
 
+	/*
+    |--------------------------------------------------------------------------
+    | OPERAÇÕES 
+    |--------------------------------------------------------------------------
+    | Manipula o evento da interface do usuário, como por exemplo o próprio CRUD
+    */
+
+	public function criar_log($id_operacao, $id_militar, $id_veiculo, $id_operador, $endereco_ip)
+	/* Para cada operação, é criado um registro de log no Banco de Dados */
+	{
+		$log = new Cad_logs;
+		$log->id_operacao = $id_operacao;
+		$log->id_militar = $id_militar;
+		$log->id_veiculo = $id_veiculo;
+		$log->id_operador = $id_operador;
+		$log->data_hora = date('Y-m-d H:i');
+		$log->endereco_ip = $endereco_ip;
+		$log->save();
+	}
+
 	public function update(Request $request, $id)
+	/* Atualiza os dados do Automovel no Banco de Dados */
 	{
 		$veiculo = Cad_automovel::findOrFail($id);
 		$dados 	= $request->all();
@@ -110,7 +136,6 @@ class VeiculosController extends Controller
 		if ($crtl) {
 			return back()->with('error', 'Já existe algum veículo ativo com esta placa!');
 		}
-
 
 		$validacao = Validator::make($dados, $regras);
 		if ($validacao->fails()) {
@@ -136,6 +161,7 @@ class VeiculosController extends Controller
 	}
 
 	public function store(Request $request)
+	/* Cria um novo Automovel no Banco de Dados */
 	{
 		$dados 	= $request->all();
 		$regras = [
@@ -185,6 +211,7 @@ class VeiculosController extends Controller
 	}
 
 	public function disabled(Request $request, $id)
+	/* Desativa o Cadastro do Veiculo */
 	{
 		$veiculo = Cad_automovel::findOrFail($id);
 		$dados 	= $request->all();
@@ -204,6 +231,7 @@ class VeiculosController extends Controller
 	}
 
 	public function enable(Request $request, $id)
+	/* Ativa o Cadastro do Veiculo */
 	{
 		$veiculo = Cad_automovel::findOrFail($id);
 		$dados 	= $request->all();
