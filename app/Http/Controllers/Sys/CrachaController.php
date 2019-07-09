9360b7577e99ad43d1ec8793a64666e6fadf833c
3º Sgt Lucas <sgtlucas@badmapcmo.eb.mil.br>
2019-07-09 09:37:32 -0400

    ajustes e permissão para gerar crachá

--- app/Http/Controllers/Sys/CrachaController.php
+++ app/Http/Controllers/Sys/CrachaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Sys;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Sys\Cad_automovel;
 use App\Model\Sys\Cad_militar;
 use App\Model\Sys\Cad_viaturas;
 use Illuminate\Support\Facades\Auth;
@@ -12,69 +13,43 @@ use DB;
 
 class CrachaController extends Controller
 {
+	/*
+    |--------------------------------------------------------------------------
+    | VIEWS 
+    |--------------------------------------------------------------------------
+    | Retornam as páginas onde é confeccionado o Crachá
+    | HTML disponível em resources/views/sys/cracha
+	*/
 
 	public function trocarCrachaIndex()
+	/* Página onde é solicitado a troca de crachá */
 	{
+		$this->authorize('trocarCracha', Cad_militar::class);
 		return view('sys.configuracoes.trocarCracha');
-	}
-
-	public function trocarCracha(Request $request)
-	{
-		$item = new Cad_militar;
-		$nameTB = $item->getTable();
-		$nameDB = DB::connection()->getDatabaseName();
-
-		$crtl = Cad_militar::where('ident_militar', '=', $request->ident_militar)->exists();
-		if (!$crtl) {
-			return response()->json(['error' => "Número de Identidade não existe!"]);
-		}
-
-		DB::beginTransaction();
-		try {
-			/*
-			** Consulta qual foi o ulitmo número registrado
-			*/
-			$value = DB::table('INFORMATION_SCHEMA.TABLES')
-				->select('AUTO_INCREMENT as last_id')
-				->where('TABLE_SCHEMA', $nameDB)
-				->where('TABLE_NAME', $nameTB)
-				->first();
-
-			/*
-			** Atualiza o ID do Militar
-			*/
-			Cad_militar::where('ident_militar', $request->ident_militar)->update(['id' => $value->last_id]);
-			/*
-			** Atualiza o número do Auto Increment da Tabela.
-			*/
-			DB::statement('ALTER TABLE ' . $nameTB . ' AUTO_INCREMENT =' . $value->last_id);
-			$this->criar_log(32, $value->last_id, NULL, Auth::user()->id, $request->getClientIp());
-		} catch (QueryException $e) {
-			DB::rollback();
-			return back()->with('error', "ERROR: " . $e->errorInfo[2]);
-		}
-		DB::commit();
-		return response()->json(['msg' => "Número do crachá alterado com sucesso!"]);
 	}
 
 	public function veiculo(Request $request, $id)
 	/*
-	Faz as consulta dos dados do militar e enviar para a view montar o crachá Pedestre/Pessoal;
+	Faz as consulta dos dados do militar e enviar para a view montar o crachá de Veículo;
 	Registra na tabela cad_logs a operação;
 	*/
 	{
-		$veiculo = DB::table('cad_automovel')
-			->select(
-				'cad_automovel.id',
-				'cad_automovel.placa',
-				'cad_automovel.baixa',
-				'cad_militar.nome_guerra',
-				'cad_posto.nome as posto_nome',
-				'cad_posto.letra',
-				'cad_marca.nome as marca',
-				'cad_modelo.nome as modelo',
-				'cad_om.nome as om_nome'
-			)
+		if (!Auth::user()->hasRole(['super-admin', 'administrador'])) {
+			return response()->json(['error' => 'Unauthorized'], 403);
+		}
+
+		$veiculo = Cad_automovel::select(
+			'cad_automovel.id',
+			'cad_automovel.placa',
+			'cad_automovel.baixa',
+			'cad_militar.nome_guerra',
+			'cad_militar.om_id',
+			'cad_posto.nome as posto_nome',
+			'cad_posto.letra',
+			'cad_marca.nome as marca',
+			'cad_modelo.nome as modelo',
+			'cad_om.nome as om_nome'
+		)
 			->join(
 				'cad_marca',
 				'cad_marca.id',
@@ -105,8 +80,16 @@ class CrachaController extends Controller
 				'=',
 				'cad_posto.id'
 			)
-			->where('cad_automovel.id', '=', $id)
-			->first();
+			->where('cad_automovel.id', '=', $id);
+
+		if (!Auth::user()->hasRole('super-admin')) {
+			$veiculo = $veiculo->where('cad_militar.om_id', Auth::user()->om_id);
+		}
+		$veiculo = $veiculo->first();
+
+		if (!$veiculo) {
+			return abort(404);
+		}
 
 		$crtl = DB::table('cad_automovel')->where('placa', '=', $veiculo->placa)
 			->where('baixa', '=', 0)
@@ -126,7 +109,7 @@ class CrachaController extends Controller
 
 	public function viatura(Request $request, $id)
 	/*
-	Faz as consulta dos dados do militar e enviar para a view montar o crachá Pedestre/Pessoal;
+	Faz as consulta dos dados do militar e enviar para a view montar o crachá de Viatura;
 	Registra na tabela cad_logs a operação;
 	*/
 	{
@@ -183,16 +166,12 @@ class CrachaController extends Controller
 		if ($vtr->vtr_cmt == 0) {
 			$viatura->letra = "X";
 		}
-
 		if ($viatura->baixa == 1) {
 			return back()->with('error', 'Viatura com STATUS DESATIVADO!');
 		}
-
 		$this->criar_log(31, NULL, $viatura->id, Auth::user()->id, $request->getClientIp());
 		return view('sys.cracha.viatura', compact('veiculo'));
 	}
-
-
 
 	public function pedestre(Request $request, $id)
 	/*
@@ -200,19 +179,22 @@ class CrachaController extends Controller
 	Registra na tabela cad_logs a operação;
 	*/
 	{
-		$militar = DB::table('cad_militar')
-			->select(
-				'cad_militar.ident_militar',
-				'cad_militar.datafile',
-				'cad_militar.id',
-				'cad_militar.status',
-				'cad_militar.posto',
-				'cad_militar.om_id',
-				'cad_militar.nome_guerra',
-				'cad_posto.nome as posto_nome',
-				'cad_posto.letra as posto_letra',
-				'cad_om.nome as om_nome'
-			)
+		if (!Auth::user()->hasRole(['super-admin', 'administrador'])) {
+			return response()->json(['error' => 'Unauthorized'], 403);
+		}
+
+		$militar = Cad_militar::select(
+			'cad_militar.ident_militar',
+			'cad_militar.datafile',
+			'cad_militar.id',
+			'cad_militar.status',
+			'cad_militar.posto',
+			'cad_militar.om_id',
+			'cad_militar.nome_guerra',
+			'cad_posto.nome as posto_nome',
+			'cad_posto.letra as posto_letra',
+			'cad_om.nome as om_nome'
+		)
 			->join(
 				'cad_posto',
 				'cad_militar.posto',
@@ -225,8 +207,17 @@ class CrachaController extends Controller
 				'=',
 				'cad_om.id'
 			)
-			->where('cad_militar.id', '=', $id)
-			->first();
+			->where('cad_militar.id', '=', $id);
+
+		if (!Auth::user()->hasRole('super-admin')) {
+			$militar = $militar->where('cad_militar.om_id', Auth::user()->om_id);
+		}
+		
+		$militar = $militar->first();
+
+		if (!$militar) {
+			return abort(404);
+		}
 
 		if ($militar->status == 2) {
 			return back()->with('error', 'Militar com STATUS DESATIVADO!');
@@ -236,9 +227,51 @@ class CrachaController extends Controller
 		return view('sys.cracha.pedestre', compact('militar'));
 	}
 
+	public function trocarCracha(Request $request)
+	/*	Operação onde é realizada a troca do ID do militar no banco de dados.
+	*	Modelo do banco é em cascade, ou seja, trocando o ID todas as foreign keys são atualizadas.
+	*/
+	{
+		$item = new Cad_militar;
+		$nameTB = $item->getTable();
+		$nameDB = DB::connection()->getDatabaseName();
+
+		$crtl = Cad_militar::where('ident_militar', '=', $request->ident_militar)->exists();
+		if (!$crtl) {
+			return response()->json(['error' => "Número de Identidade não existe!"]);
+		}
+
+		DB::beginTransaction();
+		try {
+			/*
+			** Consulta qual foi o ulitmo número registrado
+			*/
+			$value = DB::table('INFORMATION_SCHEMA.TABLES')
+				->select('AUTO_INCREMENT as last_id')
+				->where('TABLE_SCHEMA', $nameDB)
+				->where('TABLE_NAME', $nameTB)
+				->first();
+
+			/*
+			** Atualiza o ID do Militar
+			*/
+			Cad_militar::where('ident_militar', $request->ident_militar)->update(['id' => $value->last_id]);
+			/*
+			** Atualiza o número do Auto Increment da Tabela.
+			*/
+			DB::statement('ALTER TABLE ' . $nameTB . ' AUTO_INCREMENT =' . $value->last_id);
+		} catch (QueryException $e) {
+			DB::rollback();
+			return back()->with('error', "ERROR: " . $e->errorInfo[2]);
+		}
+		DB::commit();
+		$this->criar_log(32, $value->last_id, NULL, Auth::user()->id, $request->getClientIp());
+		return response()->json(['msg' => "Número do crachá alterado com sucesso!"]);
+	}
+
 	public function criar_log($id_operacao, $id_militar, $id_veiculo, $id_operador, $endereco_ip)
 	/*
-	Função para salvar a operação de gerar crachá;
+	Função para salvar as operações (Logs);
 	*/
 	{
 		$log = new Cad_logs;
